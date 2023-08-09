7847703ba93c83da9fd0b40f0a067c546d81d5e6
Igor Anoshin <ragea@mail.ru>
2023-08-09 16:17:39 +0300

    Message respond test

--- netlify/functions/update.js
+++ netlify/functions/update.js
@@ -1,4 +1,20 @@
+const axios = require("axios").default;
+
+req = `https://api.telegram.org/bot${TELEGRAM_API_TOKEN}/sendMessage`
+
+
+
+
+
+
 exports.handler = async (event) => {
     console.log("Received an update from Telegram!", event.body);
+
+    await axios.post(req, {
+        chat_id: JSON.parse(event.body).message.chat.id,
+        text: "I got your message!",
+    });
+    
     return { statusCode: 200 };
   };
+
