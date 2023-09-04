7a2db186961fe5cdaf66e3816a69553cb59b09b9
Igor Anoshin <ragea@mail.ru>
2023-09-04 15:00:56 +0300

    asd

--- controllers/elastisity.controller.js
+++ controllers/elastisity.controller.js
@@ -2,7 +2,7 @@ const db = require('../db')
 
 class ElastisityController {
     async getElasticity(req,res) {
-        const elastisity = await db.query('SELECT * FROM steels')
+        const elastisity = await db.query('SELECT * FROM steels;')
         console.log(elastisity)
     }
 }
